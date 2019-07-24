94f8ccca6c36876cc1114795f6d3f48bbbacccbc
mjaracz <michal19996@gmail.com>
2019-07-24 10:01:17 +0200

    create structure of katalog

--- api/stories/stories.routes.js
+++ api/stories/stories.routes.js
@@ -5,31 +5,50 @@ const router = require('express').Router();
 router.get('/stories/all', async (req, res) => {
   await storiesService.storiesAll(req, res)
     .then(data => data)
-    .then(stories => res.json(stories));
+    .then(stories => res.json(stories))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    });
 });
 
 router.get('/story/:id', async (req, res) => {
   await storiesService.storyByID(req, res)
     .then(story => story)
     .then(data => res.json(data))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    })
 });
 
 router.post('/story/new', async (req, res) => {
   await storiesService.storyNew(req, res)
     .then(data => res.json(data))
-    .catch(err => console.log(err))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    })
 });
 
 router.put('/story/update/:id', async (req, res) => {
   await storiesService.storyUpdate(req, res)
     .then(data => data)
     .then(storyUpdated => res.json(storyUpdated))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    })
 });
 
 router.delete(`/story/delete/:id`, async (req, res) => {
   await storiesService.storyDelete(req, res)
     .then(data => data)
     .then(deleteStory => res.json(deleteStory))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    })
 });
 
 module.exports = router;
--- api/users/user.routes.js
+++ api/users/user.routes.js
@@ -3,18 +3,30 @@ const UserService = require('./user.service');
 
 route.post('/register', async function(req, res) {
   await UserService.register(req)
-    .then(user => res.json(user));
+    .then(user => res.json(user))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    });
 });
 
 route.post('/signin', async function(req, res) {
   await UserService.signin(req.body)
     .then(user => user ? res.json(user) : res.status(400).message('username or password is incorrect'))
-    .then(token => token ? res.json(token) : res.status(400).message('username or password is incorrect'));
+    .then(token => token ? res.json(token) : res.status(400).message('username or password is incorrect'))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    })
 });
 
 route.get('/list', async function(req, res) {
   await UserService.list(req, res)
-    .then(users => res.json(users));
+    .then(users => res.json(users))
+    .catch(err => {
+      res.status(500);
+      res.statusMessage = "Internal Server Error";
+    })
 });
 
 module.exports = route;
